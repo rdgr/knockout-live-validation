7fb5934c02e68697c942e32869990bc00c54da01
Rodger <glauber@rodger.com.br>
2014-11-17 14:08:11 -0600

    Fixed error in main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 $(function() {
 
-    ko.bindingHandlers.liveValidation = {
-      init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        LiveValidation.addEvents(viewModel, valueAccessor, element);
-        LiveValidation.buildRulesContainer(viewModel, valueAccessor, element);
-        LiveValidation.updateRules(element, valueAccessor, allBindingsAccessor, viewModel);
-      },
-      update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-        LiveValidation.updateRules(element, valueAccessor, allBindings, viewModel);
-      }
-    };
+  ko.bindingHandlers.liveValidation = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+      LiveValidation.addEvents(viewModel, valueAccessor, element);
+      LiveValidation.buildRulesContainer(viewModel, valueAccessor, element);
+      LiveValidation.updateRules(element, valueAccessor, allBindingsAccessor, viewModel);
+    },
+    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+      LiveValidation.updateRules(element, valueAccessor, allBindings, viewModel);
+    }
+  };
 
-    ko.applyBindings(LiveValidationViewModel);
-
-  });
+  ko.applyBindings(LiveValidationViewModel);
 
 });
